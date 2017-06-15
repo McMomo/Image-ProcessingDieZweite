bfe24aa03ad4a701ab30bb3ffcda90b796841f4b
Unknown <moritz@bantleon.me>
2017-06-16 00:06:17 +0200

    Brace yourself. Bugs are comming!
    also jetzt ist wenigstens farbe da vorher wars noch schwarz weiß (PS: sieht man wenn man ganz nah hin zoomt aber ja viel zu klein :D) das Problem ist das die RGB werte von dem ganzen schwarzen (0,0,0) sind und ich weiß nicht warum :/

--- src/BlurFilter.java
+++ src/BlurFilter.java
@@ -1,36 +1,68 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
-public class BlurFilter extends AreaFilter{
-	
+public class BlurFilter extends AreaFilter {
+
+	// private int radius;
+	//
+	// public BlurFilter(int radius){
+	// this.radius = radius;
+	// }
+
+	private int from = 0;
+	private int to;
 	private int radius;
-	
-	public BlurFilter(int radius){
-		this.radius = radius;	
+
+	public BlurFilter(int radius) {
+		this.radius = radius;
+
 	}
 
-	
-	
-	
 	@Override
 	protected int calculate(int[] pixel, int[] maskPixel, int index, int width, int height) {
-//
-//		for ( int x = 0; x < width; x++ ) {
-//			// Get the blur radius at x, y
-//			int ra;
-//			if ( blurMask != null ) {
-//				if ( pass == 1 )
-//					ra = (int)((mask[x] & 0xff)*hRadius/255f);
-//				else
-//					ra = (int)((mask[x] & 0xff)*vRadius/255f);
-//			} else {
-//				if ( pass == 1 )
-//					ra = (int)(blurRadiusAt( x, y, width, height ) * hRadius);
-//				else
-//					ra = (int)(blurRadiusAt( y, x, height, width ) * vRadius);
-//			}
-//
-		return 0;
+		//
+		// for ( int x = 0; x < width; x++ ) {
+		// // Get the blur radius at x, y
+		// int ra;
+		// if ( blurMask != null ) {
+		// if ( pass == 1 )
+		// ra = (int)((mask[x] & 0xff)*hRadius/255f);
+		// else
+		// ra = (int)((mask[x] & 0xff)*vRadius/255f);
+		// } else {
+		// if ( pass == 1 )
+		// ra = (int)(blurRadiusAt( x, y, width, height ) * hRadius);
+		// else
+		// ra = (int)(blurRadiusAt( y, x, height, width ) * vRadius);
+		// }
+		//
+		if (index == 0) {
+			to = radius;
+		} else if (index % radius == 0) {
+
+			from = to;
+			to += radius;
+
+		}
+
+		// berechnung des durschnittswert
+		// also der Durschnittsfarbe aller Pixel + diesem Pixel;
+		int valueNum = 0;
+		Color valueCol;
+		for (int i = 0; from < to; from++) {
+			int green = ((pixel[from] >> 8) & 0xff);
+			int blue = (pixel[from] >> 16) & 0xff;
+			int red = (pixel[from] & 0xff);
+
+			int brightness = (green + blue + red) / 3;
+
+			valueNum += brightness;
+		}
+		valueNum = valueNum / radius;
+		valueCol = new Color(valueNum, valueNum, valueNum);
+
+		return valueCol.getRGB();
+
 	}
 
 }
--- src/PixelGraphicFilter.java
+++ src/PixelGraphicFilter.java
@@ -28,21 +28,38 @@ public class PixelGraphicFilter extends AreaFilter {
 
 		// berechnung des durschnittswert
 		// also der Durschnittsfarbe aller Pixel + diesem Pixel;
-		int valueNum = 0;
-		Color valueCol;
+//		int valueNum = 0;
+//		Color valueCol;
+//		for (int i = 0; from < to; from++) {
+//			int green = ((pixel[from] >> 8) & 0xff);
+//			int blue = (pixel[from] >> 16) & 0xff;
+//			int red = (pixel[from] & 0xff);
+//
+//			int brightness = (green + blue + red) / 3;
+//
+//			valueNum += brightness;
+//		}
+//		valueNum = valueNum / radius;
+//		valueCol = new Color(valueNum, valueNum, valueNum);
+//
+//		return valueCol.getRGB();
+		int green = 0, blue = 0, red = 0;
+		
+		Color commonColor;
 		for (int i = 0; from < to; from++) {
-			int green = ((pixel[from] >> 8) & 0xff);
-			int blue = (pixel[from] >> 16) & 0xff;
-			int red = (pixel[from] & 0xff);
+			green += ((pixel[from] >> 8) & 0xff);
+			blue += (pixel[from] >> 16) & 0xff;
+			red += (pixel[from] & 0xff);
 
-			int brightness = (green + blue + red) / 3;
+			
 
-			valueNum += brightness;
+			
 		}
-		valueNum = valueNum / radius;
-		valueCol = new Color(valueNum, valueNum, valueNum);
+		red /= radius; green /= radius; blue /= radius;
+		//System.out.println(red +" "+green+" "+blue);
+		commonColor = new Color(red, green, blue);
 
-		return valueCol.getRGB();
+		return commonColor.getRGB();
 	}
 
 }
